969a1e2d982fe08bbfacfe854211612605cac5a5
haruki-014 <haruki017933@gmail.com>
2026-01-30 10:22:16 +0900

    2026/1/30 appの基本設定とinstalledAppへの追加

--- TechJam/settings.py
+++ TechJam/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'calendar_app',
+    'questions',
+    'tasks',
 ]
 
 MIDDLEWARE = [
